a717e70f18df579de055ecacf447ac1b71a7772e
emin <emin@grbo.space>
2025-03-22 10:23:42 +0100

    Initial Commit

---
+++ Sawvival/SawvivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SawvivalApp.swift
+//  Sawvival
+//
+//  Created by emin on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SawvivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SawvivalTests/SawvivalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SawvivalTests.swift
+//  SawvivalTests
+//
+//  Created by emin on 22/03/2025.
+//
+
+import Testing
+@testable import Sawvival
+
+struct SawvivalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
